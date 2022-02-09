7381fe9e5b616a84dfec05493fba2235f3280eaa
EveMonte <mrpresident.evmount@gmail.com>
2022-02-09 18:00:37 +0300

    add: first lab code

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,67 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите m: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"НОД: {Calculations.NOD((uint)m, (uint)n)}");
+        
+        List<uint> numbers = Calculations.SieveEratosthenes((uint)n);
+        foreach (uint number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+
+    }
+}
+public class Calculations
+{
+    public static uint NOD(uint firstNumber, uint secondNumber)
+    {
+        if(firstNumber < secondNumber)
+        {
+            firstNumber ^= secondNumber;
+            secondNumber ^= firstNumber;
+            firstNumber ^= secondNumber;
+        }
+
+        uint reminder = 1;
+        uint previousReminder = 1;
+        reminder = firstNumber % secondNumber;
+
+        while (reminder != 0)
+        {
+            firstNumber = secondNumber;
+            secondNumber = reminder;
+            previousReminder = reminder;
+            reminder = firstNumber % secondNumber;
+        }
+        return previousReminder;
+    }
+    public static uint NOD(uint firstNumber, uint secondNumber, uint thirdNumber)
+    {
+        return NOD(thirdNumber, NOD(firstNumber, secondNumber));
+    }
+
+    public static List<uint> SieveEratosthenes(uint n)
+    {
+        var numbers = new List<uint>();
+        for (var i = 2u; i < n; i++)
+        {
+            numbers.Add(i);
+        }
+
+        for (var i = 0; i < numbers.Count; i++)
+        {
+            for (var j = 2u; j < n; j++)
+            {
+                numbers.Remove(numbers[i] * j);
+            }
+        }
+
+        return numbers;
+    }
+}
